11b29b3df0040d9411a3e8fcf8d32c3af0e6d506
Joshua Owusu <joshbillion1@gmail.com>
2023-02-17 14:01:58 +0000

    Commit auto_update

--- 03.conditionals/conditionals.go
+++ 03.conditionals/conditionals.go
@@ -11,7 +11,7 @@ func main() {
 
 	// note else should start right after the if stmt ends and not on the next line or error
 	// && means and "and" || means Or
-	if isLoggedIn || balance > 15 {
+	if isLoggedIn && balance > 15 {
 		fmt.Println("Show cart page")
 		var len, err = fmt.Println("Show cart page")
 		if err == nil {
