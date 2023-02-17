461bfea55161af56b2461c04b05d60fab59f8495
Joshua Owusu <joshbillion1@gmail.com>
2023-02-17 09:53:36 +0000

    Commit auto_update

--- 02.variables/variables.go
+++ 02.variables/variables.go
@@ -10,7 +10,12 @@ func main() {
 	var power int = 5
 	fmt.Println(superman, "My power level is", power)
 
-	var catwoman string 
-	catwoman
+	var catwoman string
+	catwoman = "I am catwoman"
+	fmt.Println(catwoman)
+
+	thor := "I am thor"
+	fmt.Println(thor)
+	
 
 }
