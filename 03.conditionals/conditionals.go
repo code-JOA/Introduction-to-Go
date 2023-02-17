acb2bcd943379619eadc7e6b44a04f0406ccd2a0
Joshua Owusu <joshbillion1@gmail.com>
2023-02-17 12:21:47 +0000

    Commit auto_update

--- 03.conditionals/conditionals.go
+++ 03.conditionals/conditionals.go
@@ -4,16 +4,14 @@ import (
 	"fmt"
 )
 
-
-
-
-function main() {
+func main() {
 	var isLoggedIn bool = true
 
+
+note else should start right after the if stmt
 	if isLoggedIn {
 		fmt.Println("Show cart page")
-	}
-	else {
+	} else {
 		fmt.Println("Show user login page")
 	}
 }
