7e3914cd44c4667db2014506b381c3e232785e2a
Joshua Owusu <joshbillion1@gmail.com>
2023-02-17 12:22:50 +0000

    Commit auto_update

--- 03.conditionals/conditionals.go
+++ 03.conditionals/conditionals.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	var isLoggedIn bool = true
+	// var isLoggedIn bool = false
+	var balance int = 10
 
-
-note else should start right after the if stmt
+	// note else should start right after the if stmt ends and not on the next line or error
 	if isLoggedIn {
 		fmt.Println("Show cart page")
 	} else {
