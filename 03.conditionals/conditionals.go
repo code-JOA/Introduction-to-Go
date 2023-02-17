538497620b0ea123a63f57da1d4c995e0ea00fce
Joshua Owusu <joshbillion1@gmail.com>
2023-02-17 12:23:53 +0000

    Commit auto_update

--- 03.conditionals/conditionals.go
+++ 03.conditionals/conditionals.go
@@ -10,7 +10,7 @@ func main() {
 	var balance int = 10
 
 	// note else should start right after the if stmt ends and not on the next line or error
-	if isLoggedIn {
+	if isLoggedIn && balance > 15 {
 		fmt.Println("Show cart page")
 	} else {
 		fmt.Println("Show user login page")
