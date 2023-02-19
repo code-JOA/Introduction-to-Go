9392d7bde99557b8d3dc56eb3d8d31f57c3f08b6
Joshua Owusu <joshbillion1@gmail.com>
2023-02-19 06:53:52 +0000

    Commit auto_update

--- 02.variables/YearOfBirth.go
+++ 02.variables/YearOfBirth.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
-  fmt.Printf("What year were you born?: ")
-  scanner.Scan()
-  input, err := strconv.ParseInt(scanner.Text(),10,64)
-  fmt.Printf("You will be %d years old at the end of 2020", 2020 - input)
-  
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("What year were you born?: ")
+	scanner.Scan()
+	input, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	fmt.Printf("You will be %d years old at the end of 2020", 2020-input)
+
 }
