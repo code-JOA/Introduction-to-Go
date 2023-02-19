f88e9255e254484af001973f4b94dc0ff8706dc2
Joshua Owusu <joshbillion1@gmail.com>
2023-02-19 06:55:57 +0000

    Commit auto_update

--- 02.variables/YearOfBirth.go
+++ 02.variables/YearOfBirth.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Printf("What year were you born?: ")
 	scanner.Scan()
 	input, err := strconv.ParseInt(scanner.Text(), 10, 64)
-	fmt.Printf("You will be %d years old at the end of 2020", 2020-input , err)
+	fmt.Printf("You will be %d years old at the end of 2020", 2023-input , err)
 
 }
+
