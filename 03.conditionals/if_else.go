5b2ddac9c444e43490f7c823a08e8f85c1093f20
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 15:07:48 +0000

    Commit auto_update

--- 03.conditionals/if_else.go
+++ 03.conditionals/if_else.go
@@ -5,5 +5,9 @@ import "fmt"
 func main() {
 	if age >= 18 {
 		fmt.Println("You can drive alone")
-	} else if age >=14 && age
+	} else if age >=14 && age<18 {
+		fmt.Println("You can drive with a Parent")
+	} else if age < 14 {
+		fmt.Println("You cannot drive. You are inderage")
+	}
 }
