dc02534bd8e9d38ff3010a7cb93713a142b08970
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 20:34:05 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/01.Pointers/Basics/01.Pointers.go
+++ 04.Pointers_and_Arrays/01.Pointers/Basics/01.Pointers.go
@@ -17,23 +17,3 @@ func main() {
 	c = 40
 	fmt.Println(a, b, *b, c)
 }
-
-package main
-
-import "fmt"
-
-func main() {
-	a := 10
-	b := &a
-	c := a
-	fmt.Println(a, b, *b, c)
-
-	a = 20
-	fmt.Println(a, b, *b, c)
-
-	*b = 30
-	fmt.Println(a, b, *b, c)
-
-	c = 40
-	fmt.Println(a, b, *b, c)
-}
