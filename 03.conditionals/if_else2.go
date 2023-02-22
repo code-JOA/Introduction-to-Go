6a4351700432c9389c6543eda5627e83a0c495ac
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 15:46:23 +0000

    Commit auto_update

--- 03.conditionals/if_else2.go
+++ 03.conditionals/if_else2.go
@@ -11,15 +11,3 @@ func main() {
 		fmt.Println("a is less than or equal to 5:")
 	}
 }
-
-
-
-func main() {
-	a := 10
-	if b := a / 2; b > 5 {
-		fmt.Println("b is bigger than 5:", b)
-	} else {
-		fmt.Println("b is less than or equal to 5:", b)
-	}
-	fmt.Println(b)
-}
