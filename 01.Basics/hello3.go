c1837fd5aede4d441ee9a0ad6062d1c34f7cf00c
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 14:49:29 +0000

    Commit auto_update

--- 01.Basics/hello3.go
+++ 01.Basics/hello3.go
@@ -15,7 +15,7 @@ func main() {
 
 	fmt.Println(s, "Has ", len(s), "words.", s2, "Has ", len(s2), " words.", s3, "Has ", len(s3), "words.")
 
-	x := "Golang to the, "
+	x := "Golang to the "
 	var r rune = 127757
 	x = x + string(r)
 	fmt.Println(x)
