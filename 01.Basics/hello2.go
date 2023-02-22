be87f418260a49dbf2621a8fc5078e3bb2fe2a48
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 14:32:36 +0000

    Commit auto_update

--- 01.Basics/hello2.go
+++ 01.Basics/hello2.go
@@ -10,12 +10,12 @@ func main() {
 	s3 := s + " " + s2
 	fmt.Println(s3)
 
-
 	x := "Go to, \n\t\"the world!\" with a backslash \\"
-	fmt.Println(s)
+	// fmt.Println(x)
+
 	b := s[0]
 	b2 := s[4]
-	fmt.Println(s, b, string(b), b2, string(b2), string(b2), string(b))
+	fmt.Println(x, b, string(b), b2, string(b2), string(b2), string(b))
 
 }
 
