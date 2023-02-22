b59e6d78fd433f9ecbe6583e5bcc665cced5b829
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 20:03:45 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/01.Pointers/05pointers.go
+++ 04.Pointers_and_Arrays/01.Pointers/05pointers.go
@@ -8,9 +8,9 @@ func main() {
 	var gamepoint float64 = 99.2
 	gameshooterRef := &gamepoint
 
-	lifebooster = gamepoint * 2.2
+	gamepoint = gamepoint * 5.2
 
 	fmt.Println(gamepoint)
-	fmt.Println(*lifeboosterRef)
+	fmt.Println(*gameshooterRef)
 
 }
