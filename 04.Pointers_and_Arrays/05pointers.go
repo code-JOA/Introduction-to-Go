3f313e0495d74bad041e6a7dad0ccba5d6b90123
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 18:51:30 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/05pointers.go
+++ 04.Pointers_and_Arrays/05pointers.go
@@ -1,16 +1,16 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// func main() {
-// 	var lifebooster float64 = 99.2
-// 	lifeboosterRef := &lifebooster
+func main() {
+	var lifebooster float64 = 99.2
+	lifeboosterRef := &lifebooster
 
-// 	lifebooster = lifebooster * 2.2
+	lifebooster = lifebooster * 2.2
 
-// 	fmt.Println(lifebooster)
-// 	fmt.Println(*lifeboosterRef)
+	fmt.Println(lifebooster)
+	fmt.Println(*lifeboosterRef)
 
-// }
+}
