3b7282a34073943c75f162e3e35e394a51d4c6b9
Joshua Owusu <joshbillion1@gmail.com>
2023-02-22 14:38:56 +0000

    Commit auto_update

--- 01.Basics/hello3.go
+++ 01.Basics/hello3.go
@@ -1,16 +1,18 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
-
 func main() {
 
-		s := "Hello, world!"
-		s2 := s[0:5]
-		s3 := s[7:12]
-		s4 := s[:5]
-		s5 := s[7:]
-		fmt.Println(s, s2, s3, s4, s5)
-	}
+	s := "Hello, world!"
+	s2 := s[0:5]
+	s3 := s[7:12]
+	s4 := s[:5]
+	s5 := s[7:]
+	fmt.Println(s, s2, s3, s4, s5)
+
+	fmt.Println(s, len(s), s2, len(s2), s3, len(s3))
+
+}
