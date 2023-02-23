fbfafd2a8ee9aeaf42dd6d967309e22c6ad0d0d9
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 10:11:28 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/01.Pointers/Basics/05.Pointers.go
+++ 04.Pointers_and_Arrays/01.Pointers/Basics/05.Pointers.go
@@ -1,18 +1,18 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func setTo10Fail(a *int) {
-	a = new(int)
-	*a = 10
-}
+// func setTo10Fail(a *int) {
+// 	a = new(int)
+// 	*a = 10
+// }
 
-func main() {
-	a := 20
-	fmt.Println(a)
-	setTo10Fail(&a)
-	fmt.Println(a)
-}
+// func main() {
+// 	a := 20
+// 	fmt.Println(a)
+// 	setTo10Fail(&a)
+// 	fmt.Println(a)
+// }
 
 
 package main
