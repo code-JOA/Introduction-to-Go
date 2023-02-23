11617900c51da479b89479716543f56eff014317
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 09:10:03 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/01.Pointers/Basics/03.Pointers.go
+++ 04.Pointers_and_Arrays/01.Pointers/Basics/03.Pointers.go
@@ -3,33 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var a = 10
+	a := 10
 	b := &a
 	c := b
 
 	a = 20
-	fmt.Println(a, b, *b)
+	fmt.Println(a, b, c)
 
 	*b = 30
-	fmt.Println(b, *b, c)
+	fmt.Println(a, *b, c)
 
 	c = 40
-	fmt.Println(b, *c)
-}
-
-
-func main() {
-	a := 10
-	b := &a
-	c := a
-	fmt.Println(a, b, *b, c)
-
-	a = 20
-	fmt.Println(a, b, *b, c)
-
-	*b = 30
-	fmt.Println(a, b, *b, c)
-
-	c = 40
-	fmt.Println(a, b, *b, c)
+	fmt.Println(a, b, *c)
 }
