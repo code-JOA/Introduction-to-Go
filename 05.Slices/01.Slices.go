ba0847ee378b486b1bbf6f78209bbd8882b9c22a
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 19:32:44 +0000

    Commit auto_update

--- 05.Slices/01.Slices.go
+++ 05.Slices/01.Slices.go
@@ -15,7 +15,6 @@ func main() {
 	// print out position range 1 and take out the last item in the Array
 	luggage = append(luggage[1 : len(luggage)-1])
 	fmt.Println(luggage)
-	
 
 	// # Here we want to create heroes, with 3 elements with an upperbound of 3 but note we can add more items with slices
 	heroes := make([]string, 3, 3)
@@ -28,4 +27,6 @@ func main() {
 
 	heroes = append(heroes, "Robin")
 	fmt.Println(heroes)
+	// # finding how much more i can add to heroes
+	fmt.Println(cap(heroes))
 }
