770dc722faa9781fbf98230e35bc2e5c431ea5fd
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 12:17:38 +0000

    Commit auto_update

---
+++ 04.Pointers_and_Arrays/02.Arrays/02.Arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	anArray := [4]int{1, 2, 4, -4}
+	twoD := [4][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}
+	threeD := [2][2][2]int{{{1, 0}, {-2, 4}}, {{5, -1}, {7, 0}}}
+
+	fmt.Println("The length of", anArray, "is", len(anArray))
+	fmt.Println("The first element of", twoD, "is", twoD[0][0])
+	fmt.Println("The length of", threeD, "is", len(threeD))
