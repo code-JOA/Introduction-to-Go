1a3d12fc319dd30d842164b7c515e44800941562
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 09:25:04 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/01.Pointers/Basics/03.Pointers.go
+++ 04.Pointers_and_Arrays/01.Pointers/Basics/03.Pointers.go
@@ -1,39 +1,33 @@
-// package main
-
-// import "fmt"
-
-// func new2() {
-// 	a := new(int)
-
-// 	fmt.Println(a)
-// 	fmt.Println(*a)
-// }
-
-
-// func setTo10(b *int) {
-// 	*b = 10
-// }
-
-// func main() {
-// 	b := 20
-// 	fmt.Println(b)
-// 	setTo10(&b)
-// 	fmt.Println(b)
-// }
-
-
 package main
 
 import "fmt"
 
-func setTo10Fail(c *int) {
-	a = new(int)
-	*a = 10
+func new() {
+	a := new(int)
+
+	fmt.Println(a)
+	fmt.Println(*a)
+}
+
+func setTo10(b *int) {
+	*b = 10
 }
 
 func main() {
-	a := 20
-	fmt.Println(a)
-	setTo10Fail(&a)
-	fmt.Println(a)
+	b := 20
+	fmt.Println(b)
+	setTo10(&b)
+	fmt.Println(b)
+}
+
+func setTo10Fail(c *int) {
+	c = new(int)
+	*c = 10
+}
+
+func new2() {
+	c := 20
+	fmt.Println(c)
+	setTo10Fail(&c)
+	fmt.Println(c)
 }
