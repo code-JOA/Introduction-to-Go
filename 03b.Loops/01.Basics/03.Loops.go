3fd4501c229f1e0390fd8e3af9608b849a7dd6f0
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 14:11:45 +0000

    Commit auto_update

--- 03b.Loops/01.Basics/03.Loops.go
+++ 03b.Loops/01.Basics/03.Loops.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+// func main() {
 
-	i := 1
-	for i < 10 {
-		fmt.Println(i)
-		i = i + i
-	}
-	fmt.Println(i)
-}
-
-// # the above can also be written this way
-// func new() {
 // 	i := 1
-// 	for {
+// 	for i < 10 {
 // 		fmt.Println(i)
 // 		i = i + i
-// 		if i > 10 {
-// 			break
-// 		}
 // 	}
 // 	fmt.Println(i)
-
 // }
+
+// # the above can also be written this way
+func new2() {
+	i := 1
+	for {
+		fmt.Println(i)
+		i = i + i
+		if i > 10 {
+			break
+		}
+	}
+	fmt.Println(i)
+
+}
