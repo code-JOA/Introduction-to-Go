d8a228834bba25585c5d73cbf99d864e8f883af1
Joshua Owusu <joshbillion1@gmail.com>
2023-02-23 14:59:17 +0000

    Commit auto_update

--- 04.Pointers_and_Arrays/02.Arrays/01.Arrays.go
+++ 04.Pointers_and_Arrays/02.Arrays/01.Arrays.go
@@ -17,4 +17,11 @@ func main() {
 	fmt.Println(restaurant[2])
 	fmt.Println(len(restaurant))
 
+	var vals [3]int
+	vals[0] = 2
+	vals[1] = 4
+	vals[2] = 6
+	var vals2 [4]int = vals
+	fmt.Println(vals, vals2)
+
 }
