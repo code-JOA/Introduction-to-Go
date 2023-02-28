5eed81203ceadee934288f7ccc9c9485cd3d7371
Joshua Owusu <joshbillion1@gmail.com>
2023-02-28 14:18:37 +0000

    updated 03.struct.go

--- 07.struct/01.struct.go
+++ 07.struct/01.struct.go
@@ -22,8 +22,28 @@ func main() {
 	fmt.Println(h)
 
 	h.A = 100
-	fmt.Print(h.A)
+	fmt.Println(h.A)
+	fmt.Printf("---------\n")
+
+
+	i := Cart{
+		A: 20,
+	}
+	var f2 Cart
+	f2 = f
+	f2.A = 100
+	fmt.Println(f2.A)
+	fmt.Println(i.A)
+
+	var f3 *Cart = &i
+	f3.A = 200
+	fmt.Println(f3.A)
+	fmt.Println(i.A)
+
 }
+
+
+
 
 
 
