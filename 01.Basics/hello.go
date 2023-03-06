526953c128caf38f02bf025669503a97c4a59dd2
Joshua Owusu <joshbillion1@gmail.com>
2023-03-06 11:40:23 +0000

    basics update

--- 01.Basics/hello.go
+++ 01.Basics/hello.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	var score int = 5
+	var name string = "Joshua"
 	// fmt.Println("Hello world")
 	fmt.Println("Hello Joshua your score is ", score, "stars")
+	fmt.Printf("Hi, %v. Welcome!" , name)
 }
